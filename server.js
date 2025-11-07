04358fc973ac165126f8b6cc79ee87d03b311191
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-11-07 10:47:56 +0200

    server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
