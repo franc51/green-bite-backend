59af377ee2fbc6ae8ce097dbd31aeba6a4dc6862
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-11-12 11:17:38 +0200

    structured folders

--- Routes/recipeRoutes.js
+++ Routes/recipeRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const recipeController = require("../controllers/recipeController");
 
 router.get("/", recipeController.getRecipes);
 router.get("/:id", recipeController.getRecipeById);
